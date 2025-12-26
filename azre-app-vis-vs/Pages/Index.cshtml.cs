e9c9fcf8b96d8ff21cde2576f4cff6db3c8af190
Vishnu Vishweshwar <VISHWESHWAR@DEV-LPT319>
2025-12-26 06:04:23 +0530

    Telemetry changes

--- azre-app-vis-vs/Pages/Index.cshtml.cs
+++ azre-app-vis-vs/Pages/Index.cshtml.cs
@@ -6,15 +6,17 @@ namespace azre_app_vis_vs.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _configuration;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public void OnGet()
         {
-
+            ViewData["Title"] = _configuration["Greeting"];
         }
     }
 }
